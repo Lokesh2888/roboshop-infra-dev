ad4b906b31c4a856cd281c5027a2238fe58be41b
Lokesh <lokesh.kumar131991@gmail.com>
2025-07-09 23:14:24 +0530

    10-SG

--- 10-SG/variables.tf
+++ 10-SG/variables.tf
@@ -13,8 +13,3 @@ variable "frontend_sg_name" {
 variable "frontend_sg_description" {
  default =  "created sg for frontend instances"
 }
-
-variable "sg_tags" {
-  type = map(string)
-  default = {} #if you give this it will work as optional
-}
