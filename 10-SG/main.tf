071973d29e3a501fe1d9f26e14d37270222db23e
Lokesh <lokesh.kumar131991@gmail.com>
2025-07-10 23:41:50 +0530

    resolve conflict

---
+++ 10-SG/main.tf
@@ -0,0 +1,11 @@
+
+module "frontend" {
+  source = "../../terraform-aws-sg"
+  #source = "git::https://github.com/Lokesh2888/terraform-aws-sg.git?ref=main"
+  project     = var.project
+  environment = var.environment
+
+  sg_name        = var.frontend_sg_name
+  sg_description = var.frontend_sg_description
+  vpc_id         = local.vpc_id
+}
